9249edc71ef5d5071a3a5285a0b8a3bcd1c7d7b9
Shreyas <shreyassinngh.official@gmail.com>
2026-08-02 18:06:47 +0530

    chore: format

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -169,11 +169,11 @@ resource "aws_security_group" "rds" {
   }
 
   ingress {
-    description     = "PostgreSQL public access"
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"] # WARNING: Consider restricting to your specific IP for security
+    description = "PostgreSQL public access"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # WARNING: Consider restricting to your specific IP for security
   }
 
   egress {
