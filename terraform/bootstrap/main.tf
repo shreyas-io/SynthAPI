6f52a2ac8fa01cda717dae83cc9618b26944c9ce
Shreyas <shreyassinngh.official@gmail.com>
2026-08-16 01:11:25 +0530

    chore: update thumbprints

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -54,6 +54,8 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
 
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+    "1b511abead59c6ce207077c0bf0e0043b1382612"
   ]
 
   tags = local.common_tags
